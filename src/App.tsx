a6f6247d1f0f7e68ac68a0cdda6a812101038d4e
Marcello Prattico <marcello@dorsia.com>
2025-02-20 10:38:50 -0500

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Dorsia Front End Interview</h1>
+      <div className="card">
+        <h2>Welcome</h2>
+        <p>You successfully started the Dorsia Front End Interview</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
